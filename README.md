13ef04350a87903912eb61503fdb0dd5b866f459
John Tannous <john.tannous97@gmail.com>
2022-02-11 22:25:04 -0800

    launching app with heroku

---
+++ README.md
@@ -0,0 +1,42 @@
+# Tech-Blog
+
+![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+    
+## Description 
+
+This project allows a user to login or signup, leave a comment, edit their comment and delete their comment.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+    
+## Installation 
+
+This project can be installed by running npm i in your terminal and installing all the need packages
+
+## Usage 
+
+Run npm start to launch the app 
+
+
+## License 
+
+This project is license under MIT
+
+## Contributing 
+
+Contributors should read the installation section.
+
+## Tests
+
+npm test
+
+## Questions
+
+If you have any questions about this projects, please contact me directly at john.tannous97@gmail.com. 
+You can view more of my projects at https://github.com/Johntannous.
---
+++ public/js/editPost.js
@@ -0,0 +1,55 @@
+const editFormHandler = async(event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#updatename').value.trim();
+    const description = document.querySelector('#updatedescription').value.trim();
+    const id = window.location.toString().split("/")[4]
+
+    if (name && description) {
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ name, description }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/post/' + id);
+        } else {
+            alert('Failed to edit post');
+        }
+    }
+};
+
+document
+    .querySelector('#editbtn')
+    .addEventListener('click', editFormHandler);
+
+const commentFormHandler = async(event) => {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('#addcomment').value.trim();
+    const post_id = window.location.toString().split("/")[4]
+    console.log(post_id, comment_text)
+
+    if (comment_text) {
+        const response = await fetch(`/api/comment`, {
+            method: 'POST',
+            body: JSON.stringify({ comment_text, post_id }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/post/' + post_id);
+        } else {
+            alert('Failed to comment post');
+        }
+    }
+};
+
+document
+    .querySelector('#btncomment')
+    .addEventListener('click', commentFormHandler);
